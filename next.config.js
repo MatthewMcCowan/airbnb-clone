397e7278bf9bb7d6bf7b2e5bb35e504b9af273bd
matthew mccowan <matthewmccowan@yahoo.com>
2021-08-20 13:39:01 -0500

    day four build with mapbox

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "a0.muscache.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibWF0dGhld21jY293YW4iLCJhIjoiY2tzamMyN3JyMmNzNDJ3bzI2czBxbG1xdyJ9.qEfU6DwI7U5nHFzxNs_GoA",
+  },
 };
